d928fc697bcbe856ce2d0bab5d6d814ee0c148ad
Deep Sea <deepsea930514@gmail.com>
2023-06-22 14:54:48 +0900

    change router

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/v1/', indexRouter)
+app.use('/v1/', indexRouter)
 app.use('*', pageNotFoundHandler)
 app.use(errorHandler)
 
