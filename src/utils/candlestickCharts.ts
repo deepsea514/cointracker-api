015d613d251d930189e2923e84fdbcef6c0082ec
Deep Sea <deepsea930514@gmail.com>
2023-08-21 09:23:31 +0000

    backup the open variable

--- src/utils/candlestickCharts.ts
+++ src/utils/candlestickCharts.ts
@@ -158,7 +158,7 @@ export function chunkIntervalSwapData(
         // to just leave this out though?
         if (previousSwap) {
           prices[timeSlotEnd].swaps.push({
-            open: previousSwap.open,
+            open: previousSwap.close,
             close: previousSwap.close,
             reserve0: previousSwap.reserve0,
             reserve1: previousSwap.reserve1,
