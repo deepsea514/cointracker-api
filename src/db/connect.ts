de7492aad6ca5e119ed2ba72669632afeea3069d
Deep Sea <deepsea930514@gmail.com>
2024-04-30 17:21:19 +0900

    wip

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,16 +1,19 @@
 import mongoose from 'mongoose'
 
 export const connect = () => {
-  const uri = `mongodb+srv://aurora:91I3vbqrXRHMFJCD@cluster0.wpw1ms6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0` // connect to local mongodb for dev and docker service for production
-  mongoose.set('strictQuery', true)
-  return mongoose.connect(uri).then(
-    () => {
-      console.log('Database connection established!')
-    },
-    (err) => {
-      {
-        console.log('Error connecting Database instance due to:', err)
-      }
-    },
-  )
+  const user = process.env.DB_USER
+  const pass = process.env.DB_PASS
+  const host = process.env.DB_HOST
+  const uri = `mongodb://${user}:${pass}@${host}` // connect to local mongodb for dev and docker service for production
+
+  return mongoose
+    .connect(uri)
+    .then((conn) => {
+      console.log(`DB connected`)
+    })
+    .catch((err) => {
+      console.log(JSON.stringify(err, null, 2))
+
+      throw new Error('Unable to connect to db')
+    })
 }
