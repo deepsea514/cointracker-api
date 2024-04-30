96ecee95a15e00217d8e47ead988f19786b54bcf
Deep Sea <deepsea930514@gmail.com>
2024-04-30 11:24:59 +0900

    update connect code

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connect = () => {
   const uri = `mongodb+srv://aurora:91I3vbqrXRHMFJCD@cluster0.wpw1ms6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0` // connect to local mongodb for dev and docker service for production
-
+  mongoose.set('strictQuery', true)
   return mongoose.connect(uri).then(
     () => {
       console.log('Database connection established!')
